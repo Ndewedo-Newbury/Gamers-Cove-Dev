35eca1a129a2848366812222626385686a8d1f62
Ndewedo Newbury <nfnewbury@dons.usfca.edu>
2025-10-10 20:54:20 -0700

    basic springboot template

---
+++ src/main/java/GamersCoveDev/GamersCoveDevApplication.java
@@ -0,0 +1,13 @@
+package GamersCoveDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersCoveDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamersCoveDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GamersCoveDev/GamersCoveDevApplicationTests.java
@@ -0,0 +1,13 @@
+package GamersCoveDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersCoveDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
