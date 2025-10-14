b6b8f520cc69b171708f1d66277e078f34ea1f7d
Ndewedo Newbury <nfnewbury@dons.usfca.edu>
2025-10-14 14:02:40 -0700

    made a slight change to review entity

--- src/main/java/GamersCoveDev/domain/entities/ReviewEntity.java
+++ src/main/java/GamersCoveDev/domain/entities/ReviewEntity.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Builder
 @Table(name = "reviews")
 public class ReviewEntity {
 
